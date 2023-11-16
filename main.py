2c4322ea46612e4ec0296d4e8b239dca61860e7a
Antun Karlovac <antun@antunkarlovac.com>
2023-11-15 18:42:52 -0800

    Replicate working

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from flask import Flask, request, jsonify
 import requests
 import json
 import time
+import uuid
+from google.cloud import storage
+import os
+os.environ["GCLOUD_PROJECT"] = 'cropscanpro'
+
 
 app = Flask(__name__)
 
@@ -18,18 +23,41 @@ headers = {
     "Authorization": f"Token {api_token}"
 }
 
+storage_client = storage.Client()
+bucket_name = 'cropscanprobucket'
+
+
 def error_json(message, details='', status_code=400):
     return jsonify({"status": "error", "message": message, "details": details}), status_code
+
+def write_read(file_contents, bucket_name, blob_name):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.content_type = 'image/png'
+
+    # Mode can be specified as wb/rb for bytes mode.
+    # See: https://docs.python.org/3/library/io.html
+    with blob.open("wb") as f:
+        f.write(file_contents)
+
 
 @app.route('/analyze', methods=['POST'])
 def analyze(request):
     # Retrieve the image URL from the request
     data = request.get_json()
-    print('data.rawImageUrl', data['rawImageUrl'])
     rawImageUrl = data['rawImageUrl']
 
     if not rawImageUrl:
         return error_json("Missing 'rawImageUrl' parameter", '', 400)
+
+    # Retrieve the image from the URL
+    image_response = requests.get(rawImageUrl)
+    # Save to GCS
+    # Generate a random filename
+    filename = 'rawImages/' + str(uuid.uuid4()) + '.png'
+    write_read(image_response.content, bucket_name, filename)
+    rawImageSavedUrl = f"https://storage.googleapis.com/cropscanprobucket/{filename}"
 
     # API request data
     api_data = {
@@ -41,7 +69,7 @@ def analyze(request):
             "box_prompt": "[0,0,0,0]",
             "image_size": 640,
             "model_name": "FastSAM-x",
-            "input_image": rawImageUrl,
+            "input_image": rawImageSavedUrl,
             "point_label": "[0]",
             "point_prompt": "[[0,0]]",
             "withContours": True,
@@ -49,24 +77,18 @@ def analyze(request):
         }
     }
 
-    print('Sending prediction request...')
     # Send the prediction request
     response = requests.post(api_url, headers=headers, data=json.dumps(api_data))
     status_url = response.json().get('urls').get('get')
-    print('Got prediction response', response.json())
-    print('status_url', status_url)
 
     if response.status_code not in [200, 201]:
         return error_json("Error sending prediction request", details, response.status_code)
 
     prediction_id = response.json().get('id')
-    print('prediction_id', prediction_id)
 
     # Check the prediction status
     while True:
         status_response = requests.get(status_url, headers=headers)
-        print('status_response', status_response)
-        print('status_response', status_response.json())
         if status_response.status_code == 200:
             result = status_response.json()
             if result.get('status') == 'succeeded':
@@ -76,7 +98,8 @@ def analyze(request):
                     {
                         "status": "success",
                         "message": "Processing complete",
-                        "rawImageUrl": "https://maps.googleapis.com/maps/api/staticmap?size=640x480&maptype=satellite&center=-19.608607231997194,-45.991371645484705&key=AIzaSyBj92vPkR0DBR6emjqohYXorNPVePsUl5o&zoom=17&scale=2",
+                        "rawImageUrl": rawImageUrl,
+                        "rawImageSavedUrl": rawImageSavedUrl,
                         "segmentedImageUrl": result.get('output'),
                         "segments": [
                             { "color": "pink", "centerCoordinates": [[123,456]], "area": 12345 },
@@ -85,6 +108,7 @@ def analyze(request):
                     }
                 ), 200
             elif result.get('status') == 'failed':
+                print(result)
                 return error_json("Error getting prediction response", '', status_response.status_code)
         else:
             return error_json("Error checking prediction status", status_response.json(), status_response.json())
